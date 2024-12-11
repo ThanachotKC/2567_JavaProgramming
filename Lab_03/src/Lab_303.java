d291d0891dd2b2a1cb8329eb7e2d9c8e691c9425
ThanachotKC <iamfarmy14@gmail.com>
2024-12-11 11:14:29 +0700

    Lab_03

--- Lab_03/src/Lab_303.java
+++ Lab_03/src/Lab_303.java
@@ -1,9 +1,36 @@
+import java.util.Scanner;
 
 public class Lab_303 {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        
+        System.out.print("Input a sentence: ");
+        String sentence = scanner.nextLine();
 
-	}
+        while (!sentence.endsWith(".")) {
+            System.out.print("Input a sentence, again :");
+            sentence = scanner.nextLine();
+        } 
+        
+        {
+            
+            sentence = sentence.substring(0, sentence.length() - 1);
 
+            
+            String[] words = sentence.split("\\s+");
+            int wordCount = words.length;
+
+            
+            int spaceCount = sentence.length() - sentence.replace(" ", "").length();
+
+            
+            System.out.println("This sentence has " + spaceCount + "spacebar.");
+            System.out.println("This sentence has " + wordCount + "word.");
+        }
+
+        
+        scanner.close();
+    }
 }
---
+++ Lab_03/src/Lab_304.java
@@ -0,0 +1,9 @@
+
+public class Lab_304 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
